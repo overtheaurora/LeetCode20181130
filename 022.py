a1804499c41a38b4daa0ff505b151d0ecd24addd
overtheaurora <38575686+overtheaurora@users.noreply.github.com>
2018-12-04 21:43:37 +0800

    Add files via upload

---
+++ 022.py
@@ -0,0 +1,15 @@
+#括号生成
+
+
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dp = [[] for i in range(n+1)]
+        dp[0].append('')
+        for i in range(n+1):
+            for j in range(i):
+                dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i-j-1]]
+        return dp[n]
